d342e923334c46a647f7975e2087ad340c44ca4e
TSIDIANY Raharison Muriel <raharisontsidiany@gmail.com>
2024-05-06 14:08:46 +0300

    Ajout de views load_data

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'app'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('load-data/', views.load_data, name='load_data'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,9 @@
+import json
+
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
@@ -8,3 +12,11 @@ def index(request):
     return render(request, 'app/index.html', {
         'path': request.path
     })
+
+
+@login_required
+@csrf_exempt
+def load_data(request):
+    data = json.loads(request.GET.get('request'))
+    print(data.get('canevas'), data)
+    return JsonResponse([], safe=False)
